a183d0aba17fda54b7639e44673e334126c72144
Hardik Khandelwal <hardikkhandelwal1804@gmail.com>
2025-05-28 10:55:37 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './Components/Navbar'
+import LandingPage from './Components/LandingPage'
+import Marque from './Components/Marque'
+import About from './Components/About'
+import Eyes from './Components/Eyes'
+import Featured from './Components/Featured'
+import Cards from './Components/Cards'
+import Footer from './Components/Footer'
+import LocomotiveScroll from 'locomotive-scroll';
+import Preload from './Components/Preloader/Preload'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const locomotiveScroll = new LocomotiveScroll();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-full min-h-screen text-white bg-zinc-900'>
+
+      <Preload />
+      <Navbar />
+      <LandingPage />
+      <Marque />
+      <About />
+      <Eyes />
+      <Featured />
+      <Cards />
+      <Footer />
+    </div>
   )
 }
 
